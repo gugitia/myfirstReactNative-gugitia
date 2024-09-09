af53af82c15da96d685bf9773e94dd10b80e4577
gugitia <gugitia@gmail.com>
2024-09-09 06:18:41 -0300

    my first commit

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+
+  function incrementar() {
+    return setNumber(number + 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: "white" }}>Olá Mundooo</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={incrementar} style={styles.button}>
+        <Text>Botao</Text>
+      </TouchableOpacity>
+      <Text style={styles.numero}>{number}</Text>
     </View>
   );
 }
@@ -13,8 +24,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "yellow",
+    marginTop: 20,
+    padding: 20,
+  },
+  numero: {
+    marginTop: 40,
+    color: "white",
   },
 });
